e9c7bb9fd0f181015c75bb349ec797626bdd0607
dorra elkahla <dorraelkahlaa@gmail.com>
2023-02-18 22:58:42 +0100

    store project

---
+++ dataaaaa.js
@@ -0,0 +1,307 @@
+  // {
+    //     "id": 1,
+    //     "name": "Red T-Shirt",
+    //     "description": "A simple, plain red t-shirt for everyday wear.",
+    //     "category": "Clothing",
+    //     "price": 14.99
+    //   },
+    //   {
+    //     "id": 2,
+    //     "name": "Blue Jeans",
+    //     "description": "A classic pair of blue jeans for casual occasions.",
+    //     "category": "Clothing",
+    //     "price": 29.99
+    //   },
+    //   {
+    //     "id": 3,
+    //     "name": "Black Leather Jacket",
+    //     "description": "A stylish and durable leather jacket for colder weather.",
+    //     "category": "Clothing",
+    //     "price": 89.99
+    //   },
+    //   {
+    //     "id": 4,
+    //     "name": "White Sneakers",
+    //     "description": "A comfortable and versatile pair of white sneakers.",
+    //     "category": "Footwear",
+    //     "price": 39.99
+    //   },
+    //   {
+    //     "id": 5,
+    //     "name": "Leather Belt",
+    //     "description": "A high-quality leather belt for a polished look.",
+    //     "category": "Accessories",
+    //     "price": 19.99
+    //   },
+    //   {
+    //     "id": 6,
+    //     "name": "Floral Dress",
+    //     "description": "A beautiful, flowy dress with a floral pattern for a feminine touch.",
+    //     "category": "Clothing",
+    //     "price": 49.99
+    //   },
+    //   {
+    //     "id": 7,
+    //     "name": "Wool Scarf",
+    //     "description": "A cozy and warm wool scarf for chilly weather.",
+    //     "category": "Accessories",
+    //     "price": 24.99
+    //   },
+    //   {
+    //     "id": 8,
+    //     "name": "Black Pumps",
+    //     "description": "A stylish pair of black pumps for a classic look.",
+    //     "category": "Footwear",
+    //     "price": 49.99
+    //   },
+    //   {
+    //     "id": 9,
+    //     "name": "Leather Gloves",
+    //     "description": "A pair of leather gloves for a sophisticated touch.",
+    //     "category": "Accessories",
+    //     "price": 29.99
+    //   },
+    //   {
+    //     "id": 10,
+    //     "name": "Blue Blouse",
+    //     "description": "A beautiful blue blouse with a flattering fit.",
+    //     "category": "Clothing",
+    //     "price": 34.99
+    //   },
+    //   {
+    //     "id": 11,
+    //     "name": "Denim Skirt",
+    //     "description": "A trendy denim skirt for a casual look.",
+    //     "category": "Clothing",
+    //     "price": 24.99
+    //   },
+    //   {
+    //     "id": 12,
+    //     "name": "Sunglasses",
+    //     "description": "A stylish pair of sunglasses for sunny days.",
+    //     "category": "Accessories",
+    //     "price": 14.99
+    //   },
+    //   {
+    //     "id": 13,
+    //     "name": "Black Leather Boots",
+    //     "description": "A pair of black leather boots for a sleek look.",
+    //     "category": "Footwear",
+    //     "price": 59.99
+    //   },
+    //   {
+    //     "id": 14,
+    //     "name": "Striped T-Shirt",
+    //     "description": "A casual striped t-shirt for everyday wear.",
+    //     "category": "Clothing",
+    //     "price": 19.99
+    //   },
+    //   {
+    //     "id": 21,
+    //     "name": "Red Leather Jacket",
+    //     "description": "Stylish red leather jacket with zippered pockets",
+    //     "category": "Jackets", 
+    //     "price": 129.99
+    // },
+    // {
+    //     "id": 22, 
+    //     "name": "Black Ankle Boots", 
+    //     "description": "Sleek black ankle boots with buckle detail",
+    //     "category": "Shoes",
+    //     "price": 89.99
+    // },
+    // {
+    //     "id": 23, 
+    //     "name": "Navy Blue Turtleneck",
+    //     "description": "Soft and comfortable turtleneck sweater in navy blue",
+    //     "category": "Sweaters",
+    //     "price": 59.99
+    // },
+    // {
+    //     "id": 24,
+    //     "name": "Leather Messenger Bag", 
+    //     "description": "Stylish leather messenger bag with adjustable strap", 
+    //     "category": "Bags", 
+    //     "price": 149.99
+    // },
+    // {
+    //     "id": 25, 
+    //     "name": "Red Plaid Scarf", 
+    //     "description": "Cozy red plaid scarf made with soft wool", 
+    //     "category": "Scarves", 
+    //     "price": 39.99
+    // },
+    // {
+    //     "id": 26, 
+    //     "name": "Black Leather Gloves", 
+    //     "description": "Classic black leather gloves with cashmere lining", 
+    //     "category": "Gloves", 
+    //     "price": 49.99
+    // },
+    // {
+    //     "id": 27, 
+    //     "name": "Gray Tweed Blazer", 
+    //     "description": "Sophisticated gray tweed blazer with elbow patches", 
+    //     "category": "Jackets", 
+    //     "price": 179.99
+    // },
+    // {
+    //     "id": 28, 
+    //     "name": "Leather Chelsea Boots", 
+    //     "description": "Stylish and comfortable leather Chelsea boots in brown", 
+    //     "category": "Shoes", 
+    //     "price": 109.99
+    // },
+    // {
+    //     "id": 29, 
+    //     "name": "Navy Blue Peacoat", 
+    //     "description": "Classic navy blue peacoat made with warm wool blend", 
+    //     "category": "Jackets", 
+    //     "price": 199.99
+    // },
+    // {
+    //     "id": 30, 
+    //     "name": "Leather Crossbody Bag", 
+    //     "description": "Chic and versatile leather crossbody bag with adjustable strap", 
+    //     "category": "Bags", 
+    //     "price": 129.99
+    // },
+    // {
+    //     "id": 31, 
+    //     "name": "Red Cashmere Scarf", 
+    //     "description": "Luxurious red cashmere scarf with fringed edges", 
+    //     "category": "Scarves", 
+    //     "price": 89.22
+    // },
+    // {
+    //     "id": 32, 
+    //     "name": "Black Leather Belt", 
+    //     "description": "Sleek and durable black leather belt with silver buckle", 
+    //     "category": "Accessories", 
+    //     "price": 39.99
+    // },
+    // {
+    //     "id": 33, 
+    //     "name": "Navy Blue Wool Sweater", 
+    //     "description": "Warm and comfortable navy blue wool sweater with ribbed detailing", 
+    //     "category": "Sweaters", 
+    //     "price": 79.99
+    // },
+    // {
+    //     "id": 34, 
+    //     "name": "Leather Laptop Bag", 
+    //     "description": "Spacious and durable leather laptop bag with multiple compartments", 
+    //     "category": "Bags", 
+    //     "price": 179.99
+    // },
+    // {
+    //     "id": 35, 
+    //     "name": "Gray Wool Scarf", 
+    //     "description": "Cozy gray wool scarf with fringed edges", 
+    //     "category": "Scarves", 
+    //     "price": 34.99
+    // },
+    // {
+    //     "id": 36, 
+    //     "name": "Leather Dress Shoes", 
+    //     "description": "Elegant leather dress shoes with cap toe and brogue detailing", 
+    //     "category": "Shoes",
+    //     "price": 130.00
+    // },{
+    //     "id": 121,
+    //     "name": "Red Leather Belt",
+    //     "description": "A sleek and stylish leather belt in a vibrant red color.",
+    //     "category": "Accessories",
+    //     "price": 29.99
+    //   },
+    //   {
+    //     "id": 122,
+    //     "name": "Gray Beanie Hat",
+    //     "description": "A soft and cozy beanie hat in a classic gray color.",
+    //     "category": "Accessories",
+    //     "price": 14.99
+    //   },
+    //   {
+    //     "id": 123,
+    //     "name": "Black Leather Boots",
+    //     "description": "Stylish and comfortable boots made with genuine leather.",
+    //     "category": "Shoes",
+    //     "price": 89.99
+    //   },
+    //   {
+    //     "id": 124,
+    //     "name": "Tartan Scarf",
+    //     "description": "A warm and cozy scarf in a classic tartan pattern.",
+    //     "category": "Accessories",
+    //     "price": 19.99
+    //   },
+    //   {
+    //     "id": 125,
+    //     "name": "Black Leather Gloves",
+    //     "description": "A pair of stylish and comfortable leather gloves.",
+    //     "category": "Accessories",
+    //     "price": 39.99
+    //   },
+    //   {
+    //     "id": 126,
+    //     "name": "Blue Denim Shorts",
+    //     "description": "A pair of stylish and comfortable shorts in a classic denim fabric.",
+    //     "category": "Clothing",
+    //     "price": 24.99
+    //   },
+    //   {
+    //     "id": 127,
+    //     "name": "Black Leather Backpack",
+    //     "description": "A stylish and functional backpack made with genuine leather.",
+    //     "category": "Bags",
+    //     "price": 69.99
+    //   },
+    //   {
+    //     "id": 128,
+    //     "name": "Brown Leather Wallet",
+    //     "description": "A high-quality leather wallet with multiple compartments and a sleek design.",
+    //     "category": "Accessories",
+    //     "price": 49.99
+    //   },
+    //   {
+    //     "id": 129,
+    //     "name": "Oversized Sunglasses",
+    //     "description": "Stylish and on-trend sunglasses with oversized frames.",
+    //     "category": "Accessories",
+    //     "price": 29.99
+    //   },
+    //   {
+    //     "id": 130,
+    //     "name": "Black Leather Jacket",
+    //     "description": "A sleek and stylish leather jacket in a classic black color.",
+    //     "category": "Clothing",
+    //     "price": 119.99
+    //   },
+    //   {
+    //     "id": 131,
+    //     "name": "Cotton T-Shirt",
+    //     "description": "A comfortable and versatile cotton t-shirt in a classic white color.",
+    //     "category": "Clothing",
+    //     "price": 14.99
+    //   },
+    //   {
+    //     "id": 132,
+    //     "name": "Blue Denim Jacket",
+    //     "description": "A stylish and on-trend denim jacket in a classic blue color.",
+    //     "category": "Clothing",
+    //     "price": 89.99
+    //   },
+    //   {
+    //     "id": 133,
+    //     "name": "Tan Leather Belt",
+    //     "description": "A high-quality leather belt in a classic tan color.",
+    //     "category": "Accessories",
+    //     "price": 29.99
+    //   },
+    //   {
+    //     "id": 134,
+    //     "name": "Leather Crossbody Bag",
+    //     "description": "A stylish and functional crossbody bag made with genuine leather.",
+    //     "category": "Bags", 
+    //     "price": 80.05 
+    //   }]
